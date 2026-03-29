a322756d060a613585d2d5bb9ed88b12dd52ac48
abdullahahadi <abdullah.ahadi78@gmail.com>
2026-03-29 06:33:34 +0430

    count frequency of each characte

--- script.js
+++ script.js
@@ -5,5 +5,16 @@ const height = 600;
 let svg = d3
     .select("body")
     .append("svg")
-    .attr("width",  width)
+    .attr("width", width)
     .attr("height", height)
+
+
+d3.select("textarea").on("input", e => {
+
+    const frequency = {};
+    e.target.value.split("").forEach(char => {
+        let currentCount = frequency[char] || 0;
+        frequency[char] = currentCount + 1;
+    });
+    console.log(frequency)
+})
